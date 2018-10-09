b2ce2c91c552d25d04c53c1e092985c4f7ddf8d8
Shreya Srivastava <shreya.srivastava963@gmail.com>
2018-10-09 20:24:46 +0530

    Add files via upload

---
+++ Caps-on-off.ps1
@@ -0,0 +1,16 @@
+﻿$keyboard_object = New-Object -ComObject WScript.Shell
+Function Caps_Lock_Off
+{
+$caps_lock_key_status = [System.Windows.Forms.Control]::IsKeyLocked('CapsLock')
+if ($caps_lock_key_status -eq $True)
+    {
+        $keyboard_object.SendKeys("{CAPSLOCK}")
+        Start-Sleep -Seconds 1
+    }
+    else
+    {
+     $keyboard_object.SendKeys("{CAPSLOCK}")
+        Start-Sleep -Seconds 1
+    }
+}
+Caps_Lock_Off
